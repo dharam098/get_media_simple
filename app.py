76080b1a78062e21e515e2a615419211ad295b32
dharmendra maheshwari <69689715+dharam098@users.noreply.github.com>
2023-07-06 21:01:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import base64
 from py1337x import py1337x
 # import pyratebay
 torrents = py1337x(proxy='1337x.to', cache='py1337xCache', cacheTime=500)
-# import libtorrent as lt
+import libtorrent as lt
 from requests.adapters import HTTPAdapter, Retry
 # from bs4 import BeautifulSoup
 
