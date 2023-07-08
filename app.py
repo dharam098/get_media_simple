58dfdb2441a0813cc368b0b26916322d729e48b3
dharmendra maheshwari <69689715+dharam098@users.noreply.github.com>
2023-07-09 05:14:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def suggest_movie_names(string):
             if match == movie['title']:
                 suggested_names.append(remove_special_characters(f"{match} {movie['year']}"))
                 suggested_names.append(remove_special_characters(match)
-                break
+				       break
                 
     suggested_names.append(string)
     return suggested_names
